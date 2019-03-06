cb3dcb9140f3bd48887818fd185eaff7037907fb
alanclarke <alan@qubit.com>
2019-03-06 17:31:31 +0000

    Fix year count

--- test/test-how-long.js
+++ test/test-how-long.js
@@ -11,131 +11,103 @@ describe('how long', function () {
   ms.days = ms.hours * 24
   ms.weeks = ms.days * 7
 
-  it('should compute years', function () {
+  it('should count years', function () {
     var date1 = new Date()
-    var date2 = new Date(date1.getTime())
-    date2.setFullYear(date2.getFullYear() + 1)
-    expect(howLong(date1, date2)).to.eql({
-      years: 1,
-      months: 0,
-      weeks: 0,
-      days: 0,
-      hours: 0,
-      minutes: 0,
-      seconds: 0,
-      milliseconds: 0
-    })
+    for (var i = 0; i < 100; i++) {
+      var date2 = new Date(date1.getTime())
+      date2.setFullYear(date1.getFullYear() + i)
+      expect(howLong(date1, date2, ['years'])).to.eql({
+        years: i
+      })
+    }
   })
 
-  it('should compute months', function () {
+  it('should count months', function () {
     var date1 = new Date()
-    var date2 = new Date(date1.getTime())
-    date2.setMonth(date2.getMonth() + 1)
-    expect(howLong(date1, date2)).to.eql({
-      years: 0,
-      months: 1,
-      weeks: 0,
-      days: 0,
-      hours: 0,
-      minutes: 0,
-      seconds: 0,
-      milliseconds: 0
-    })
+    for (var i = 0; i < 100; i++) {
+      var date2 = new Date(date1.getTime())
+      date2.setMonth(date1.getMonth() + i)
+      expect(howLong(date1, date2, ['months'])).to.eql({
+        months: i
+      })
+    }
   })
 
-  it('should compute weeks', function () {
+  it('should count weeks', function () {
     var date1 = new Date()
-    var date2 = new Date(date1.getTime())
-    date2.setTime(date2.getTime() + ms.weeks)
-    expect(howLong(date1, date2)).to.eql({
-      years: 0,
-      months: 0,
-      weeks: 1,
-      days: 0,
-      hours: 0,
-      minutes: 0,
-      seconds: 0,
-      milliseconds: 0
-    })
+    for (var i = 0; i < 100; i++) {
+      var date2 = new Date(date1.getTime() + (ms.weeks * i))
+      expect(howLong(date1, date2, ['weeks'])).to.eql({
+        weeks: i
+      })
+    }
   })
 
-  it('should compute days', function () {
+  it('should count days', function () {
     var date1 = new Date()
+    for (var i = 0; i < 100; i++) {
+      var date2 = new Date(date1.getTime() + (ms.days * i))
+      expect(howLong(date1, date2, ['days'])).to.eql({
+        days: i
+      })
+    }
+  })
+
+  it('should count hours', function () {
+    var date1 = new Date()
+    for (var i = 0; i < 100; i++) {
+      var date2 = new Date(date1.getTime() + (ms.hours * i))
+      expect(howLong(date1, date2, ['hours'])).to.eql({
+        hours: i
+      })
+    }
+  })
+
+  it('should count minutes', function () {
+    var date1 = new Date()
+    for (var i = 0; i < 100; i++) {
+      var date2 = new Date(date1.getTime() + (ms.minutes * i))
+      expect(howLong(date1, date2, ['minutes'])).to.eql({
+        minutes: i
+      })
+    }
+  })
+
+  it('should count seconds', function () {
+    var date1 = new Date()
+    for (var i = 0; i < 100; i++) {
+      var date2 = new Date(date1.getTime() + (ms.seconds * i))
+      expect(howLong(date1, date2, ['seconds'])).to.eql({
+        seconds: i
+      })
+    }
+  })
+
+  it('should count milliseconds', function () {
+    var date1 = new Date()
+    for (var i = 0; i < ms.weeks; i += ms.hours) {
+      var date2 = new Date(date1.getTime() + i)
+      expect(howLong(date1, date2, ['milliseconds'])).to.eql({
+        milliseconds: i
+      })
+    }
+  })
+
+  it('should count all the things', function () {
+    var date1 = new Date(0)
     var date2 = new Date(date1.getTime())
-    date2.setTime(date2.getTime() + ms.days)
+    date2.setFullYear(date1.getFullYear() + 1)
+    date2.setMonth(date1.getMonth() + 1)
+    for (var unit in ms) date2.setTime(date2.getTime() + ms[unit])
     expect(howLong(date1, date2)).to.eql({
-      years: 0,
-      months: 0,
-      weeks: 0,
       days: 1,
-      hours: 0,
-      minutes: 0,
-      seconds: 0,
-      milliseconds: 0
-    })
-  })
-
-  it('should compute hours', function () {
-    var date1 = new Date()
-    var date2 = new Date(date1.getTime())
-    date2.setTime(date2.getTime() + ms.hours)
-    expect(howLong(date1, date2)).to.eql({
-      years: 0,
-      months: 0,
-      weeks: 0,
-      days: 0,
       hours: 1,
-      minutes: 0,
-      seconds: 0,
-      milliseconds: 0
-    })
-  })
-
-  it('should compute minutes', function () {
-    var date1 = new Date()
-    var date2 = new Date(date1.getTime())
-    date2.setTime(date2.getTime() + ms.minutes)
-    expect(howLong(date1, date2)).to.eql({
-      years: 0,
-      months: 0,
-      weeks: 0,
-      days: 0,
-      hours: 0,
+      milliseconds: 1,
       minutes: 1,
-      seconds: 0,
-      milliseconds: 0
-    })
-  })
-
-  it('should compute seconds', function () {
-    var date1 = new Date()
-    var date2 = new Date(date1.getTime())
-    date2.setTime(date2.getTime() + ms.seconds)
-    expect(howLong(date1, date2)).to.eql({
-      years: 0,
-      months: 0,
-      weeks: 0,
-      days: 0,
-      hours: 0,
-      minutes: 0,
+      months: 1,
       seconds: 1,
-      milliseconds: 0
-    })
-  })
-
-  it('should compute milliseconds', function () {
-    var date1 = new Date()
-    var date2 = new Date(date1.getTime())
-    date2.setTime(date2.getTime() + ms.milliseconds)
-    expect(howLong(date1, date2)).to.eql({
-      years: 0,
-      months: 0,
-      weeks: 0,
-      days: 0,
-      hours: 0,
-      minutes: 0,
-      seconds: 0,
-      milliseconds: 1
+      weeks: 1,
+      years: 1
     })
   })
 
@@ -145,27 +117,33 @@ describe('how long', function () {
     expect(howLong(date1, date2, 'seconds')).to.eql({
       seconds: 61
     })
+
     expect(howLong(date1, date2, ['seconds', 'minutes'])).to.eql({
       seconds: 1,
       minutes: 1
     })
+
     expect(howLong(date1, date2, ['seconds', 'minutes', 'milliseconds'])).to.eql({
       seconds: 1,
       minutes: 1,
       milliseconds: 1
     })
     date2 = new Date(date1.getTime() + ms.days)
+
     expect(howLong(date1, date2, ['hours'])).to.eql({
       hours: 24
     })
+
     expect(howLong(date1, date2, ['days', 'hours'])).to.eql({
       days: 1,
       hours: 0
     })
     date2 = new Date(date1.getTime() + ms.weeks)
+
     expect(howLong(date1, date2, ['days'])).to.eql({
       days: 7
     })
+
     expect(howLong(date1, date2, ['weeks', 'days'])).to.eql({
       weeks: 1,
       days: 0
@@ -175,6 +153,11 @@ describe('how long', function () {
       months: 2,
       weeks: 2,
       days: 5
+    })
+
+    expect(howLong(new Date(2017, 11, 1), new Date(2019, 10, 1), ['years', 'months'])).to.eql({
+      years: 1,
+      months: 11
     })
   })
 })
